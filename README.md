1e76aa3862a9160dcc5778259ebe3d568621b798
BOMBYASCHER <bombyascher@gmail.com>
2023-03-24 13:32:36 +0300

    Add GCD game and update README

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 <a href="https://codeclimate.com/github/BOMBYASCHER/frontend-project-44/maintainability"><img src="https://api.codeclimate.com/v1/badges/7afc4e8157ecb1f19455/maintainability" /></a>
 [![asciicast](https://asciinema.org/a/2G0fSHkRDavblruEnrO5K5PLX.svg)](https://asciinema.org/a/2G0fSHkRDavblruEnrO5K5PLX)
 [![asciicast](https://asciinema.org/a/nR9bvkXkSUJuP7rSvGUObaRfu.svg)](https://asciinema.org/a/nR9bvkXkSUJuP7rSvGUObaRfu)
+[![asciicast](https://asciinema.org/a/SgjfIbouEVeSHpoGrJN5vy7eQ.svg)](https://asciinema.org/a/SgjfIbouEVeSHpoGrJN5vy7eQ)
---
+++ src/games/brain-gcd.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+import { congratsMessage, getGameRounds, passMessage, questionMessage, randomNumber, stopMessage } from "../index.js";
+import getName from "../cli.js";
+
+const gcdGame = () => {
+    const userName = getName();
+    console.log("Find the greatest common divisor of given numbers.");
+    let i = 0;
+    while (i < getGameRounds()) {
+        let firstNumber = randomNumber(0, 100);
+        let secondNumber = randomNumber(0, 100);
+        let maxNumber = Math.max(firstNumber, secondNumber);
+        let correctAnswer;
+        if (firstNumber == 0 || secondNumber == 0) {
+            correctAnswer = maxNumber;
+        } else {
+            correctAnswer = getGcd(firstNumber, secondNumber);
+        }
+        let expression = `${firstNumber} ${secondNumber}`;
+        let userAnswer = questionMessage(expression);
+        if (correctAnswer == userAnswer) {
+            passMessage();
+            i++;
+        } else {
+            stopMessage(userName, correctAnswer, userAnswer);
+            break;
+        }
+    }
+    if (i == getGameRounds()) {
+        congratsMessage(userName);
+    }
+}
+
+export default gcdGame;
+
+const getGcd = (firstNumber, secondNumber) => {
+    if (secondNumber == 0) {
+        return firstNumber;
+    }
+    return getGcd(secondNumber, firstNumber % secondNumber)
+}
